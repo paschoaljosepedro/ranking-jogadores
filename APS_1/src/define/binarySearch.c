3ec772944ff0b1adb97bdc856d848c14dd15b244
Pon <juaneduardomodesto@gmail.com>
2025-05-14 20:14:21 -0300

    JEM:[feat]:binary search
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   APS_1/src/define/binarySearch.c
    
     Changes not staged for commit:
    	modified:   APS_1/notas

---
+++ APS_1/src/define/binarySearch.c
@@ -0,0 +1,40 @@
+#include "../header/ranking.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct RANKING *binarySearchRanking(const struct DESCRITOR *desc_stru,
+                                    int targetScore) {
+  if (desc_stru == NULL || desc_stru->head == NULL) {
+    fprintf(stderr, "Erro: Descritor vazio ou nulo\n");
+    return NULL;
+  }
+
+  int left = 0;
+  int right = desc_stru->size - 1;
+  struct RANKING *result = NULL;
+
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+
+    struct RANKING *current = desc_stru->head;
+    for (int i = 0; i < mid && current != NULL; i++) {
+      current = current->next;
+    }
+
+    if (current == NULL) {
+      break;
+    }
+
+    if (current->score == targetScore) {
+      result = current;
+      right = mid - 1;
+    } else if (current->score < targetScore) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return result;
+}
